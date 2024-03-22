873f3fc9ee90a6abc2807bdaec612be4616b7ec5
Alina <alina.stolbikova.com>
2024-03-22 14:57:10 +0100

    is palindrome. array.

---
+++ two_sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[target - nums[i]] !== undefined) {
+      return [map[target - nums[i]], i];
+    } else {
+      map[nums[i]] = i;
+    }
+  }
+};
+
+console.log(twoSum([2, 7, 5, 3], 9));
