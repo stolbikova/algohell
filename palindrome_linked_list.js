177ead09e24fc3b9e0eab9b9299f74649c182529
Alina <alina.stolbikova@gmail.com>
2024-03-22 14:30:07 +0100

    finished palindrome task.

--- palindrome_linked_list.js
+++ palindrome_linked_list.js
@@ -10,12 +10,34 @@
  * @return {boolean}
  */
  var isPalindrome = function(head) {
+    // find the middle
     let slow = head;
     let fast = head.next;
-    while (fast) {
+    while (fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
     }
+
+    // reverse the second part
+    let prev = null;
+    while (slow) {
+        const tmp = slow.next;
+       slow.next = prev;
+       prev = slow;
+       slow = tmp;
+    }
+
+    //  Compare 2 halfs
+    let left = head;
+    let right = prev;
+    while (left && right) {
+        if (left.val !== right.val) {
+            return false
+        }
+        left = left.next;
+        right = right.next;
+    }
+    return true;
 };
 
-isPalindrome([1,2,2,1]);
+console.log(isPalindrome([1,1,2,1]));
