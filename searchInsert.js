b9d1f05d8adddf2f29a2f2286777971047158e69
Alina <alina.stolbikova@gmail.com>
2024-05-29 13:55:32 +0200

    remove duplicates, longest palindrome.

--- searchInsert.js
+++ searchInsert.js
@@ -5,19 +5,21 @@
  */
 var searchInsert = function (nums, target) {
   let start = 0;
-  let end = nums.length;
-  let mid = Math.floor(nums.length / 2);
-  while (mid) {
+  let end = nums.length - 1;
+  while (start <= end) {
+    const mid = Math.floor((end + start) / 2);
     if (target === nums[mid]) {
       return mid;
     } else if (nums[mid] < target) {
-      end = mid;
+      start = mid + 1;
     } else if (nums[mid] > target) {
-      start = mid;
+      end = mid - 1;
     }
-    mid = Math.floor((end - start) / 2);
   }
-  return mid;
+  return start;
 };
 
 console.log(searchInsert([1, 3, 5, 6], (target = 5)));
+
+// 3
+// 1 2 3 4 5 6 7
