e8ffd571a8adff4c341c80cf2034fe4564107c64
Alina <alina.stolbikova.com>
2024-05-29 13:55:32 +0200

    remove duplicates, longest palindrome.

---
+++ removeDuplicates.js
@@ -0,0 +1,18 @@
+var removeDuplicates = function (nums) {
+  let unique = 0;
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[unique]) {
+      unique++;
+      nums[unique] = nums[i];
+    }
+  }
+  return unique + 1;
+};
+
+// Test case
+let nums = [1, 1, 2];
+let len = removeDuplicates(nums);
+console.log(len); // Output: 2
+
+// 1 1 2
+//   1 2
