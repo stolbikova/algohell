2e816a54e03a2a42715884264c8fc84837b3072c
Alina <alina.stolbikova@gmail.com>
2024-05-30 14:45:22 +0200

    length of longest substrinig.

---
+++ lengthOfLongestSubstring.js
@@ -0,0 +1,18 @@
+function lengthOfLongestSubstring(s) {
+  const map = new Map();
+  let maxLength = 0;
+  let left = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    if (map.has(s[right])) {
+      left = Math.max(map.get(s[right]) + 1, left);
+    }
+    map.set(s[right], right);
+
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(lengthOfLongestSubstring("alina"));
