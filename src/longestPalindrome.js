3c117df83f8e6b5ec9e033a22c15f6d6d61ace08
Alina <alina.stolbikova@gmail.com>
2024-06-27 17:02:44 +0200

    longest palindrome refactoring.

--- src/longestPalindrome.js
+++ src/longestPalindrome.js
@@ -4,7 +4,6 @@ var longestPalindrome = function (string) {
 
   var centeredPalindrome = function (left, right) {
     while (left >= 0 && right < length && string[left] === string[right]) {
-      //expand in each direction.
       left--;
       right++;
     }
@@ -14,11 +13,7 @@ var longestPalindrome = function (string) {
 
   for (var i = 0; i < length - 1; i++) {
     var oddPal = centeredPalindrome(i, i + 1);
-
     var evenPal = centeredPalindrome(i, i);
-
-    if (oddPal.length > 1) console.log("oddPal: " + oddPal);
-    if (evenPal.length > 1) console.log("evenPal: " + evenPal);
 
     if (oddPal.length > result.length) result = oddPal;
     if (evenPal.length > result.length) result = evenPal;
