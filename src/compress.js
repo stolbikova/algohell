a5957d1ab555176c4cb8161781c7b51ecd14e443
Alina <alina.stolbikova@gmail.com>
2024-08-02 17:33:23 +0200

    compress.

---
+++ src/compress.js
@@ -0,0 +1,29 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function (chars) {
+  let prev = chars[0];
+  let lastIndex = 1;
+
+  for (let i = 1; i < chars.length; i++) {
+    const char = chars[i];
+
+    if (char === prev) {
+      if (!Number.isNaN(parseInt(chars[i - 1], 10))) {
+        chars[i - 1] = (parseInt(chars[i - 1], 10) + 1).toString();
+      } else {
+        chars[i] = "2";
+        lastIndex++;
+      }
+    } else {
+      lastIndex++;
+    }
+    prev = char;
+  }
+
+  return lastIndex;
+};
+
+console.log(compress(["a", "a", "b", "b", "c", "c", "c"]));
+// ["a","2","b","2","c","3"]
